4401e804d92eb224205daa3d3002aa6909f63219
Andile45 <manganyeandile@gmail.com>
2026-01-19 13:46:32 +0200

    docs(types): define profile interface for user data

---
+++ src/common/types/profile.ts
@@ -0,0 +1,12 @@
+export interface Profile{
+    id:string;
+    auth_id:string; //supabase Auth uid
+    name:string;
+    surname:string;
+    contact_number:string;
+    email:string;
+    address?:string;
+    card_last4?:string;
+    role:'user' | 'admin';
+    created_at:string
+}
