1b712b36216a70a2ef90938c750ba794919e0f14
Andile45 <manganyeandile@gmail.com>
2026-01-20 11:45:24 +0200

    feat(ui): add reusable Card component with consistent design

---
+++ src/mobile/components/Card.tsx
@@ -0,0 +1,35 @@
+import {View , StyleSheet,ViewStyle} from 'react-native';
+import { colors } from '../theme/colors';
+import { Children } from 'react';
+
+interface CardProps{
+    children:React.ReactNode;
+    style?:ViewStyle;
+};
+
+export const Card = ({children,style}:CardProps)=>{
+
+    return(
+        <View style={[styles.card,style]}>
+            {children}
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    card:{
+       backgroundColor:colors.background,
+       borderRadius:24,
+       padding:20,
+
+    // IOS Shadows
+
+    shadowColor:'#000',
+    shadowOffset:{width:0 , height:6},
+    shadowOpacity:0.08,
+    shadowRadius:12,
+    
+    // Android Shadow
+    elevation:6,
+    },
+});
