e336eac0b31301cc32dd940c0508dc876ed51dcb
Andile45 <manganyeandile@gmail.com>
2026-01-25 15:03:31 +0200

    feat(navigation): update app layout structure

---
+++ src/mobile/app/_layout.tsx
@@ -0,0 +1,5 @@
+// This file is for Expo Router compatibility
+// Currently using React Navigation directly in App.tsx
+// This can be used if migrating to Expo Router later
+
+export { default } from '../App';
