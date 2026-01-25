7865c4579eb949df03a4137b2710f7b92ce78e35
Andile45 <manganyeandile@gmail.com>
2026-01-25 15:03:33 +0200

    feat(store): update store configuration with new slices

---
+++ src/mobile/store/index.ts
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import authReducer from './slices/authSlice';
+import cartReducer from './slices/cartSlice';
+import orderReducer from './slices/orderSlice';
+import menuReducer from './slices/menuSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+    order: orderReducer,
+    menu: menuReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Typed hooks
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
